405ae3880c9bd1df56274a4d2639575be6128766
Marius Obreja <marius.obreja9@gmail.com>
2021-08-16 10:35:16 +0100

    feat add mock data seeder

--- model/index.js
+++ model/index.js
@@ -1,10 +1,14 @@
-const { Sequelize, DataTypes } = require('sequelize');
+require('dotenv').config();
 
+const { Sequelize, DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_OWNER,
   process.env.DB_PASSWORD,
+  // 'adventour',
+  // 'bissy',
+  // 'Teiubesc17.,',
   {
     host: 'localhost',
     dialect: 'postgres'
@@ -17,22 +21,22 @@ const Booking = require('./booking')(sequelize, DataTypes);
 const Player = require('./player')(sequelize, DataTypes);
 
 User.hasMany(Booking, {
-  foreignKey: 'userId'
+  // foreignKey: 'userId'
 });
 Booking.belongsTo(User);
 
 Tour.hasMany(Booking, {
-  foreignKey: 'tourId'
+  // foreignKey: 'tourId'
 });
 Booking.belongsTo(Tour);
 
 User.hasMany(Player, {
-  foreignKey: 'userId'
+  // foreignKey: 'userId'
 });
 Player.belongsTo(User);
 
 Booking.hasMany(Player, {
-  foreignKey: 'userId'
+  // foreignKey: 'userId'
 });
 Player.belongsTo(Booking);
 
@@ -40,21 +44,10 @@ Player.belongsTo(Booking);
   try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
-    await sequelize.sync({force: true});
-    await Tour.create({
-      title: 'Jack The Ripper',
-      location: 'Central London',
-      rating: 5,
-      genre: 'history',
-      briefDescription: 'Explore London while solving a famous murderer',
-      distance: 3,
-      price: 30,
-      description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at lorem lobortis, efficitur diam eu, bibendum sem. Morbi vel imperdiet mi, in euismod arcu. Suspendisse quis laoreet eros. Sed blandit molestie dui sed scelerisque. Vivamus aliquam tincidunt diam sit amet maximus. Etiam nisi leo, ultrices quis vulputate ut, accumsan ut velit. Duis non faucibus lectus, nec maximus velit. Maecenas felis orci, finibus ac enim ut, faucibus gravida leo. Curabitur finibus felis non ipsum lobortis, in tempor diam volutpat.',
-      images: 'https://static.dw.com/image/17907040_6.jpg'
-    });
+    await sequelize.sync();
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
 })();
 
-module.exports = { User, Tour, Booking, Player };
+module.exports = { User, Tour, Booking, Player, sequelize };

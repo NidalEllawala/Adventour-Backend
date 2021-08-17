093e02094ed59698debe487fe51ff6c469e79098
NidalEllawala <arthurnidal@gmail.com>
2021-08-17 17:55:49 +0100

    fix syntax error with cors

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const app = new express();
-// const cors = require('cors');
+const cors = require('cors');
 const { router } = require('./routes/router');
 
 const PORT = process.env.PORT || 3000;
 
-// app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(router);
 

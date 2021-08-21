5284c954c25caa369cfcdf0370d7a8581aca32d1
NidalEllawala <arthurnidal@gmail.com>
2021-08-21 11:39:33 +0100

    refactor joinGame route to use booking password rather than booking id

--- controller/player.js
+++ controller/player.js
@@ -1,18 +1,19 @@
 const { Player, Booking } = require('../model/index');
 
 const joinGame = async (req, res) => {
-  const { bookingId, userId } = req.body;
+  const { password, userId } = req.body;
   console.log('Join Game');
   try {
-    const booking = await Booking.findOne({ where: { id: bookingId } });
+    const booking = await Booking.findOne({ where: { password: password } });
+    console.log(booking);
     const playerInGame = await Player.findOne({
-      where: { BookingId: bookingId, UserId: userId }
+      where: { BookingId: booking.id, UserId: userId }
     });
     if (playerInGame) {
       res.status(201).send('You have already joined');
     } else if (playerInGame === null && booking.joined < booking.partySize) {
       await Player.create({
-        BookingId: bookingId,
+        BookingId: booking.id,
         UserId: userId
       });
       booking.joined += 1;

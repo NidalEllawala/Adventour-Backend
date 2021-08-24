e5c34b4eb0ef3e8245ba6b5aa1a6e1c6812d0998
Marius Obreja <marius.obreja9@gmail.com>
2021-08-24 12:18:55 +0100

    bugs fixed seed

--- controller/clues.js
+++ controller/clues.js
@@ -1,15 +1,16 @@
 const { CharingCrossCharmer } = require('../model/index');
 //alter controoler to search table based on tour name
+
 const getNextClue = async (req, res) => {
   try {
     const nextClue = await CharingCrossCharmer.findOne({
       where: { id: req.body.clueNumber }
     });
+    console.log(nextClue);
     res.status(200).json(nextClue);
   } catch (error) {
     console.log(error);
   }
 };
-
 
 module.exports = { getNextClue };
